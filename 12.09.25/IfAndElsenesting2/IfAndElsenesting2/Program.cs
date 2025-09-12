60b49896e957caeb912d0b6df1e6ede386a5805b
MSI\ingva <inde999@gmail.com>
2025-09-12 11:40:12 +0300

    IfAndElseNesting2

---
+++ 12.09.25/IfAndElsenesting2/IfAndElsenesting2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace IfAndElsenesting2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teha üks if ja else nestimine iseseisvalt");
+            Console.WriteLine("Teise else if-i sisse panna if ja else nestimine");
+            //teema saad ise valida
+
+            int y = 10;
+
+            if (y == 12)
+            {
+                Console.WriteLine("Y võrdub 12");
+            }
+            else if (y > 20)
+            {
+                if (y == 25)
+                {
+                    Console.WriteLine("Y võrdub 25");
+                }
+                else
+                {
+                    Console.WriteLine("Y on vahemikus 21 kuni 24 ja 26 kuni lõpmatus");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Mingid kahtlased numbrid jälle");
+            }
+        }
+    }
+}
