6d158088525efa8212d24aa5c32efb15609cbbb6
MSI\ingva <inde999@gmail.com>
2025-09-12 09:23:51 +0300

    IfAndElse

---
+++ 12.09.25/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda nimi");
+
+            //siin on muutuja nimega name ja selle sisse saab lisada väärtust
+            string name = Console.ReadLine();
+
+            //if ja else kontrollib, kas name muutuja on tühi või mitte
+            if (name != "")
+            {
+                Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("Sisestasid enda nime");
+                Console.WriteLine(name);
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR. Nime ei sisestatud");
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+            }
+        }
+    }
+}
