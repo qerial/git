8d4d3b2fc5d757ec4029ae1a467d73fa74093eed
MSI\ingva <inde999@gmail.com>
2025-09-26 11:35:28 +0300

    IfAndElse

---
+++ 04. 26.09.25/IfAndElseEnterNumber/IfAndElseEnterNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace IfAndElseEnterNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta numbrid 1, 2 või 3");
+
+            string nr = Console.ReadLine();
+            int number = int.Parse(nr);
+            //teha vooskeem e flowchart sellest rakendusest 
+            if (number == 1)
+            {
+                Console.WriteLine("Sisestasid numbri 1");
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("Sisestasid numbri 2");
+            }
+            else if (number == 3)
+            {
+                Console.WriteLine("Sisestasid numbri 3");
+            }
+            else
+            {
+                Console.WriteLine("Kahtlane number. ERROR!!!");
+            }
+        }
+    }
+}
