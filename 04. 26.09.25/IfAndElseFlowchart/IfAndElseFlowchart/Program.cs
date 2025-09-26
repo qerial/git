dc6ad21f109b536a0f5e85a6e3ae7a2de7df6b01
MSI\ingva <inde999@gmail.com>
2025-09-26 10:42:08 +0300

    IfAndElseFlowchart

---
+++ 04. 26.09.25/IfAndElseFlowchart/IfAndElseFlowchart/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace IfAndElseFlowchart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string muutuja = Console.ReadLine();
+
+            if (muutuja == "")
+            {
+                Console.WriteLine("Ei sisestanud nime");
+            }
+            else
+            {
+                Console.WriteLine("Sisestasid nime " + muutuja);
+            }
+        }
+    }
+}
