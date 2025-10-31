3348a09736ae90d033fc0af927b87af72361b820
Opilane TTHK <opilane@tthk.ee>
2025-10-31 10:27:11 +0200

    switch

---
+++ 05.31.10.25/SwitchMethodCall/SwitchMethodCall/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SwitchMethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+
+
+        static void CarCoupe()
+        {
+            Console.WriteLine("This is a coupe car. ");
+        }
+        static void Truck()
+        {
+            Console.WriteLine("This is a truck. ");
+        }
+    }
+}
---
+++ 05.31.10.25/SwitchMultipleCases1/SwitchMultipleCases/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SwitchMultipleCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta väärtus");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                //sisestad konsooli väärtuse
+                //loeb sisestatud väärtuse stringina
+                //tuleb kasutada switchi
+                //esimeses cases on väärtused "Intro to C#", "Variables",
+                //"Data Types" ja vastuseks Basic
+                case "Intro to C#" or "Variables" or "Data Types":
+                    Console.WriteLine("Basic");
+                    break;
+                //teises cases on väärtused "OOP", "Classes"
+                //"Objects" ja vastuseks intermediate
+                case "OOP" or "Classes" or "Objects":
+                    Console.WriteLine("Intermediate");
+                    break;
+                //kolmandas cases on väärtused "Asynchronus Programming, "LINQ",
+                //"Delegates" ja vastuseks Advanced
+                case "Asynchronus Programming" or "LINQ" or "Delegates":
+                    Console.WriteLine("Advanced");
+                    break;
+                default:
+                    Console.WriteLine("Unknown course");
+                    break;
+            }
+        }
+    }
+}
