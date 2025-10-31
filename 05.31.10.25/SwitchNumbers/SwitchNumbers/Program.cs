71107457c6a2ae3c29a16c392baf180952381840
Opilane TTHK <opilane@tthk.ee>
2025-10-31 11:41:15 +0200

    w

---
+++ 05.31.10.25/SwitchNumbers/SwitchNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SwitchNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //peab saama sisestada numbrit ja muudab selle int andmetüübiks
+            int number = Convert.ToInt32((string) Console.ReadLine());
+            //tuleb teha switch, kus on esimene case 10, teine on 20 ja kolmas on 30
+            //kindlasti tukeb ja default lõppu teha
+            switch (number)
+            {
+            case 10:
+                Console.WriteLine("Esimene switch!");
+                break;
+            
+            case 20:
+                Console.WriteLine("Teine switch!");
+                break;
+
+            case 30:
+                Console.WriteLine("Kolmas switch!");
+                break;
+            default:
+                Console.WriteLine("ERROR! switchi pole tuvastatud");
+                break ;
+
+            }
+        }
+    }
+}
