b4142edbe6a8a015f5f0633972bd708cea8ce519
Opilane TTHK <opilane@tthk.ee>
2025-11-21 09:31:57 +0200

    uus kood

---
+++ 07. 21.11.25/TryCatchSwitch/TryCatchSwitch/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.NetworkInformation;
+
+namespace TryCatchSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesa number!");
+            
+
+            //tee switch, kus on ainult kaks casei
+            //ja valikud on 1 ja 2 
+            //default haru ei ole vajalik
+            try
+            {
+
+                int nr = int.Parse(Console.ReadLine());
+
+                switch (nr)
+                {
+                    case 1:
+                        Console.WriteLine("Valisid 1 ");
+                        break;
+                    case 2:
+                        Console.WriteLine("Valisid 2");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Palun sisesta number!");
+            }
+            catch (OverflowException) 
+            {
+                Console.WriteLine("Sisestatud number on liiga suur või liiga väike!");
+            }   
+        }
+    }
+}
