53f2768e060cf89a5186bc9cf20aeb19e529d165
Opilane TTHK <opilane@tthk.ee>
2025-11-21 10:27:55 +0200

    uus kood tunnis

---
+++ 07. 21.11.25/WhileDo/WhileDo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace WhileDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While Do");
+
+            int number = 1;
+
+            do
+            {
+                Console.WriteLine(number);
+                //suurendab numbri ühe võrra e number + 1
+                number++;
+                //kui number on väiksem või võrdne 5-ga siis jätkab tsüklit
+            } while (number <= 5);
+        }
+    }
+}
