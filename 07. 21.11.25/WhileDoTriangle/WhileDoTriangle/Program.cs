e66f40c5742cf4ab47d12295046dffe9c666d421
Opilane TTHK <opilane@tthk.ee>
2025-11-21 11:37:45 +0200

    UUS KOOD

---
+++ 07. 21.11.25/WhileDoTriangle/WhileDoTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace WhileDoTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolmnurk");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i <= n)
+            {
+                Console.WriteLine();
+                int j = 1;
+                while (j <= i)
+                {
+                    Console.Write(j + " ");
+                    j++;
+                }
+                i++;
+            
+
+
+            //otsida internetist while koodiga kolmnurga kujundeid e tegemis viise
+            //kui kood on leitud, siis kirjutada see siia alla 
+            //teha vooskeem
+            }
+            Console.ReadKey();
+        }
+    }
+}
