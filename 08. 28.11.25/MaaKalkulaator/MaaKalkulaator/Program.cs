ed3ff8e35f9218fbc17017b4202ab2e352c2ec8e
Opilane TTHK <opilane@tthk.ee>
2025-11-28 11:23:01 +0200

    Update Program.cs

--- 08. 28.11.25/MaaKalkulaator/MaaKalkulaator/Program.cs
+++ 08. 28.11.25/MaaKalkulaator/MaaKalkulaator/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //Meil on münt, mille diameeter on 25,75 mm
+            //Arvutada välja Maa ümbermõõt ja mitu münti
+            //Mahub järjestikku ümber Maa
+            //Arvutuses tohib kasutada ainult mündi diameetrit ja Maa raadiust
+            int MaaRaadius = 637100000;
+            double MündiRaadius = 25.75;
+            double maaümbermõõt = 2 * Math.PI * MaaRaadius;
+            Console.WriteLine("Maa ümbermõõt " + maaümbermõõt + "mm" );
+            double Mündiümbermõõt = 2 * Math.PI * MündiRaadius;
+            Console.WriteLine("Mündi ümbermõõt on " + Mündiümbermõõt + "mm");
+            double MündidÜmbermaa = maaümbermõõt / Mündiümbermõõt;
+            Console.WriteLine("Maa ümber mahub " + MündidÜmbermaa + " Münti");
+
+
         }
     }
 }
