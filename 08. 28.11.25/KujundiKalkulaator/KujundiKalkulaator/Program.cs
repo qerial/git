fe9a763790d048f9ca88c22f8646ffe317ecd0f4
Opilane TTHK <opilane@tthk.ee>
2025-11-28 11:03:15 +0200

    uus code
    oppisin kalkulaatorit tegema

---
+++ 08. 28.11.25/KujundiKalkulaator/KujundiKalkulaator/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace KujundiKalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Ruudu sees asub ring. Ringi raadius on 3 ühikut.
+            //Leia ja väljasta ekraanile ruudu pindala ja ümbermõõt
+
+            int roundRadius = 3; 
+            int diameeter = roundRadius * 2;
+            double SquareS = diameeter * diameeter;
+            double SquareP = diameeter * 4;
+                Console.WriteLine("Diameeter on " + diameeter);
+                Console.WriteLine("Ruudu ümbermõõt on " + SquareS);
+                Console.WriteLine("Ruudu pindala on " + SquareP);
+
+            //arvutada ringi pindala ja ümbermõõt 
+            double CircleC = 2 * Math.PI * roundRadius;
+
+            Console.WriteLine("Ringi ümbermõõt on " + CircleC);
+
+            double CircleS = Math.PI * (roundRadius * roundRadius);
+
+            Console.WriteLine("Ringi Pindala on " + CircleS);
+
+
+        }
+    }
+}
