19ad3714b0c92b0726d8fb2d933d20fef138e2d9
Opilane TTHK <opilane@tthk.ee>
2025-12-05 11:07:21 +0200

    Update Program.cs

--- 09.05.12.25/ForLoopAdding/ForLoopAdding/Program.cs
+++ 09.05.12.25/ForLoopAdding/ForLoopAdding/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("|--------------------------------------|");
+            Console.WriteLine("|Täisarvude kokku liitmine kuni kümneni|");
+            Console.WriteLine("|--------------------------------------|");
+            Console.WriteLine("\n\n");
+            int j, sum = 0;
+
+            for (j = 1; j <= 10; j++)
+            {
+                sum = sum + j;
+            Console.WriteLine("Summa on: " + sum);
+
+            }
         }
     }
 }
