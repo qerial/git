d29ac80ba028d94ba32ced3e9094f3d8afc25ef5
Opilane TTHK <opilane@tthk.ee>
2025-12-05 09:58:05 +0200

    new code again

---
+++ 09.05.12.25/For LoopEndless/For LoopEndless/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace For_LoopEndless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ushort i = 0;
+
+            {
+
+                //kui on 10 rida konsoolis, siis lõpetab ära
+
+                for (; ; )
+                {
+                    Console.WriteLine("i väärtus on " + i);
+                    i++;
+                    if (i == 11)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
