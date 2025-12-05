ce5821a509ecbdcf6efa25ea7d6d360d7ee34963
Opilane TTHK <opilane@tthk.ee>
2025-12-05 11:42:18 +0200

    CODE

---
+++ 09.05.12.25/ForKillKoll/ForKillKoll/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ForKillKoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Eelnevaid teadmisi kasutades kirjuta programm, mis väljastaks:
+            //kill - koll
+            //kill - koll
+            //killadi - koll
+            //kill -koll
+            //kill -koll
+            //killadi - koll
+            //kill -koll
+            //kill -koll
+            //killadi - koll
+            //kill -koll
+
+            Console.WriteLine("Sisesta korduste arv");
+            int arv = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < arv; i++)
+            {
+                for (int j = 0; j < 2; j++) ;
+                Console.WriteLine("kill -koll");
+                {
+                    for (int x = 0; x < 1; x++)
+                    {
+                        Console.WriteLine("killadi - koll");
+                    }
+                }
+                Console.WriteLine("kill - koll");
+            }
+        }
+    }
+}
