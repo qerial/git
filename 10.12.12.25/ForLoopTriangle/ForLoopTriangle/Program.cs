cd241599da350b884eecfbdb113c4dabf904f269
Opilane TTHK <opilane@tthk.ee>
2025-12-12 11:10:42 +0200

    koodike

---
+++ 10.12.12.25/ForLoopTriangle/ForLoopTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Channels;
+
+namespace ForLoopTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("tärnidest kolmnurk e poolik püramiid");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Sisesta ridade arv: ");
+            int i, j, rows;
+            rows = Convert.ToInt32(Console.ReadLine());
+
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
