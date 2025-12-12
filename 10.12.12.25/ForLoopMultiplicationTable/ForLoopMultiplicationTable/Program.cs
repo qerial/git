a1036b9213cd57a3cfca6d661f0b68c08d6cdc8c
Opilane TTHK <opilane@tthk.ee>
2025-12-12 10:50:19 +0200

    hh

---
+++ 10.12.12.25/ForLoopMultiplicationTable/ForLoopMultiplicationTable/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ForLoopMultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Korrutustabel");
+
+            int j, i, n;
+
+            Console.WriteLine("Sisesta tabelisse number, millest soovid alustada");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Korrutustabel ühest kuni {0}", n);
+            Console.WriteLine("\n");
+
+            for (i = 1; i <= 10; i++)
+            {
+                for (j = 1; j <= n; j++)
+                {
+                    if (j <= n - i)
+                    {
+                        Console.Write("{0}x{1} = {2}, ", j, i, i * j);
+                    }
+                    else
+                    {
+                        Console.Write("{0}x{1} = {2}, ", j, i, i * j);
+
+                    }
+                }
+            }
+        }
+    }
+}
