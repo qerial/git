c17d506180b2ff00915d76632d082da8700af57e
Opilane TTHK <opilane@tthk.ee>
2025-12-12 11:40:06 +0200

    r

---
+++ 10.12.12.25/ForLoopPyramid/ForLoopPyramid/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ForLoopPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loome tärnidest püramiidi: \n");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Sisesta suurus: ");
+
+            int i, j, n;
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i <= n; i++)
+            {
+                for (j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+
+                for (j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+
+                Console.Write("\n");
+            }
+            for (i = n - 1;i >= 1; i--)
+            { 
+                for (j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+
+                for (j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+
+                Console.Write("\n");
+              
+            }
+        }
+    }
+}
