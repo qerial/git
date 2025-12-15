18a743325cfca1a02987e39fd01917ee72d1f0f0
Opilane TTHK <opilane@tthk.ee>
2025-12-15 09:58:03 +0200

    2 tundi 2 tood

---
+++ 11.15.12.25/DecimalToBinary/DecimalToBinary/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DecimalToBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decimal to binary");
+
+            Console.WriteLine("Sisesta number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string binaryNumber = "";
+
+            while (num > 0)
+            {
+                int reminder = num % 2;
+                binaryNumber = reminder + binaryNumber;
+                num /= 2;
+            }
+            Console.WriteLine(binaryNumber);
+        }
+    }
+}
---
+++ 11.15.12.25/FibonacciFor/FibonacciFor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace FibonacciFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n");
+            Console.WriteLine("--------------------------------\n\n");
+            Console.WriteLine("Näita esimest Fibonacci seeriat");
+            Console.WriteLine("--------------------------------\n\n");
+            Console.WriteLine("Sisesta number");
+            int prv = 0, pre = 1, trm, i, n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Siin on Fibonacci seeria {0} kuni \n", n);
+
+            Console.Write($"{0}  {1}", prv, pre);
+
+            for (i = 3; i <= n; i++)
+            {
+                trm = prv + pre;
+                Console.Write($"  {trm}  ");
+                prv = pre;
+                pre = trm;
+
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
