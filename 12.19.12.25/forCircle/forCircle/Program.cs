7215f0f66daf0d1fcd3af8ab4ad6f846e38ca558
Opilane TTHK <opilane@tthk.ee>
2025-12-19 10:23:39 +0200

    KLLKLK

---
+++ 12.19.12.25/forCircle/forCircle/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace forCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            double thickness = 0.4;
+            char symbol = '*';
+
+
+            do
+            {
+                Console.WriteLine("Enter radius:::::");
+                if (!double.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+                {
+                    Console.WriteLine("Radius have to be positive number");
+                }
+            }
+            while (radius <= 0);
+            Console.WriteLine();
+            double rIn = radius - thickness, r0ut = radius + thickness;
+
+            for (double y = radius; y >= -radius; --y)
+            {
+                for (double x = -radius; x < r0ut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= r0ut * r0ut)
+                    {
+                        Console.Write(symbol);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
