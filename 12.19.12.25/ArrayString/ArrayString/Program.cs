298c4a5abf4fd9e0553422810e95f44ebae9fb06
Opilane TTHK <opilane@tthk.ee>
2025-12-19 11:21:11 +0200

    kood

---
+++ 12.19.12.25/ArrayString/ArrayString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Channels;
+
+namespace ArrayString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Euroopa üks pealinnadest on ";
+            Console.WriteLine("Tuple");
+            
+            //näide 1
+            var cities = Tuple.Create("Tallinn", "Pariis", "Rooma", "London", "Helsinki");
+            //tuple all on viis erinevat väärtust ja sellepärast saab antud
+            //juhul 5 item-it sisestada
+            Console.WriteLine(sentence + cities.Item5);
+            Console.WriteLine("---------------------");
+
+            //näide 2 
+            string[] city = { "Tallinn", "Pariis", "Rooma", "London", "Helsinki" };
+            for (int i = 0; i < city.Length; i++)
+            {
+                    Console.WriteLine(sentence + city[i]);
+            }
+
+            Console.WriteLine("---------------------");
+
+            //näide 3
+            Console.WriteLine(sentence + city[2]);
+            Console.WriteLine("---------------------");
+
+            //näide 4
+            Random number = new Random();
+            int randomNumber = number.Next(0, 4);
+            Console.WriteLine(sentence + city[randomNumber]);
+            Console.WriteLine("---------------------");
+        }
+    }
+}
---
+++ 12.19.12.25/MethodCall/MethodCall/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        //kutsuda meetod välja
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            SayHello();
+            SayGoodbye();
+        }
+        static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
