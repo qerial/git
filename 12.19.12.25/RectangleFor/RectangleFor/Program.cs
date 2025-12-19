9e136637cf8fcc88f22e3c3cf9af3016f95be5a2
Opilane TTHK <opilane@tthk.ee>
2025-12-19 11:40:36 +0200

    kood

---
+++ 12.19.12.25/RectangleFor/RectangleFor/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace RectangleFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta ristküliku kahe külje mõõdud");
+            Console.WriteLine("Pikkus");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Laius");
+            double Width = double.Parse(Console.ReadLine());
+
+            for (int row = 0; row < length; row++)
+            {
+                for (int column = 0; column <= Width; column++)
+                {
+                    string mark;
+                    if (row == column || row + column <= +1)
+                    {
+                        mark = "*";
+                    }
+                    else
+                    {
+                        mark = "*";
+                    }
+                    Console.Write(mark);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
