b1968b88d6d84feeb4737243235cdbee70b3766c
Opilane TTHK <opilane@tthk.ee>
2026-01-16 11:22:51 +0200

    g

---
+++ 16.16.01.26/FindWord/FindWord/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FindWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta otsitav sõna");
+            Console.WriteLine("Kasuta formaati:  s--a." +
+                "Kus - tähistab tundmatut tähte");
+
+            string choice = Console.ReadLine();
+
+            WordFinder(choice);
+        }
+
+        private static void WordFinder(string choice)
+        {
+            Regex regularExp = new Regex(@"\b" + choice.Replace("-", "\\w"), 
+                RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:/Users/opilane/Desktop/Lemmad.txt", Encoding.Default))
+                {
+                    int LineNumber = 0;
+
+                    while (sr.EndOfStream == false)
+                    {
+                        string line = sr.ReadLine();
+
+                        if (line.Length == choice.Length)
+                        {
+                            MatchCollection matchCollection = regularExp.Matches(line);
+
+                            if (matchCollection.Count > 0)
+                            {
+                                LineNumber++;
+                                Console.WriteLine(LineNumber + " " + line);
+                            }
+                        }
+                    }
+                    sr.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ei ole olemas sellise pikkusega sõna");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
