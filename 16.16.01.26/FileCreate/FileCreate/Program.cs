68e89eb966894de332754069e8292b09842c5291
Opilane TTHK <opilane@tthk.ee>
2026-01-16 10:44:00 +0200

    FileCreate

---
+++ 16.16.01.26/FileCreate/FileCreate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Channels;
+
+namespace FileCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjutab faili läbi konsooli");
+
+            string filePath = "C:/Users/opilane/Desktop/WriteToFile.txt";
+            string input = Console.ReadLine();
+
+            File.WriteAllText(filePath, input);
+        }
+    }
+}
